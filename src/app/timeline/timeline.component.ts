b689fd664b0dd11f58ad5eb5da9844f1365cf33f
Billy Wolfington <willwolfram18@gmail.com>
2019-01-30 20:53:39 -0800

    Auto generated dates

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-timeline",
     templateUrl: "./timeline.component.html",
     styleUrls: ["./timeline.component.scss"]
 })
-export class TimelineComponent implements OnInit
+export class TimelineComponent
 {
+    private readonly numEvents = 15;
 
-    constructor() { }
+    readonly events: Date[] = [];
+    readonly today = new Date();
+    readonly yesterday = new Date(
+        this.today.getFullYear(),
+        this.today.getMonth(),
+        this.today.getDate() - 1
+    );
 
-    ngOnInit()
-    {
+    constructor() {
+        for (let i = 0; i < this.numEvents; i++) {
+            this.events.push(
+                this.randomDate(
+                    this.yesterday,
+                    this.today
+                )
+            );
+        }
     }
 
+    private get timelineDuration() {
+        return this.today.getTime() - this.yesterday.getTime();
+    }
+
+    randomDate(start: Date, end: Date) {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    }
+
+    computePosition(date: Date) {
+        return `${((this.today.getTime() - date.getTime()) / this.timelineDuration) * 100}%`;
+    }
 }
