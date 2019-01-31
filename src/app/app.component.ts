fc3fb4bfa85e020a2aeee1bbc3027f062384a4ca
Billy Wolfington <willwolfram18@gmail.com>
2019-01-30 20:06:35 -0800

    Minor code style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'timeline-with-angular';
+export class AppComponent
+{
+    title = "timeline-with-angular";
 
-  onClick() {
-    alert("Hello world!");
-  }
+    onClick()
+    {
+        alert("Hello world!");
+    }
 }
