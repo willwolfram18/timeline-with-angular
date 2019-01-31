ba82f473c6b2caf4920ed9d40ac0622525511ea3
Billy Wolfington <willwolfram18@gmail.com>
2019-01-30 21:20:07 -0800

    Moved date generation up to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,41 @@ import { Component } from "@angular/core";
 })
 export class AppComponent
 {
-    title = "timeline-with-angular";
+    private readonly numEvents = 15;
+
+    readonly events: TimelineEvent[] = [];
+    readonly today = new Date();
+    readonly yesterday = new Date(
+        this.today.getFullYear(),
+        this.today.getMonth(),
+        this.today.getDate() - 1
+    );
+
+    constructor() {
+        for (let i = 0; i < this.numEvents; i++) {
+            this.events.push({
+                description: `Description ${i}`,
+                title: `Title ${i}`,
+                timestamp: this.randomDate(
+                    this.yesterday,
+                    this.today
+                )
+            });
+        }
+        this.events.sort((e1, e2) => e2.timestamp.getTime() - e1.timestamp.getTime());
+    }
+
+    get timestamps() {
+        return this.events.map(e => e.timestamp);
+    }
+
+    randomDate(start: Date, end: Date) {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    }
+}
+
+interface TimelineEvent {
+    timestamp: Date;
+    title: string;
+    description: string;
 }
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,42 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-timeline",
     templateUrl: "./timeline.component.html",
     styleUrls: ["./timeline.component.scss"]
 })
-export class TimelineComponent
+export class TimelineComponent implements OnInit
 {
-    private readonly numEvents = 15;
+    @Input()
+    events: Date[] | null = null;
 
-    readonly events: Date[] = [];
-    readonly today = new Date();
-    readonly yesterday = new Date(
-        this.today.getFullYear(),
-        this.today.getMonth(),
-        this.today.getDate() - 1
-    );
+    @Input()
+    endOfTimeline: Date | null = null;
 
-    constructor() {
-        for (let i = 0; i < this.numEvents; i++) {
-            this.events.push(
-                this.randomDate(
-                    this.yesterday,
-                    this.today
-                )
-            );
+    @Input()
+    startOfTimeline: Date | null = null;
+
+    ngOnInit() {
+        if (!this.events) {
+            throw new Error("No events.");
+        }
+
+        if (!this.endOfTimeline) {
+            throw new Error("No end of timeline.");
+        }
+
+        if (!this.startOfTimeline) {
+            throw new Error("No start of timeline.")
         }
     }
 
-    private get timelineDuration() {
-        return this.today.getTime() - this.yesterday.getTime();
-    }
-
-    randomDate(start: Date, end: Date) {
-        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-    }
-
     computePosition(date: Date) {
-        return `${((this.today.getTime() - date.getTime()) / this.timelineDuration) * 100}%`;
+        return `${((this.endOfTimeline.getTime() - date.getTime()) / this.timelineDuration) * 100}%`;
+    }
+
+    private get timelineDuration() {
+        return this.endOfTimeline.getTime() - this.startOfTimeline.getTime();
     }
 }
